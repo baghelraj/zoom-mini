9027fcc4f0aa83bca92bbc6561b5707f935203ec
Raj <rajender.singh@hoote.com>
2023-07-24 20:11:48 +0530

    adding writing video output code

--- subscriber.py
+++ subscriber.py
@@ -12,6 +12,11 @@ client_channel = client.pubsub()
 
 # Let's subscribe to other user's channel.
 client_channel.subscribe("user_1")
+
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (360, 640))
  
 # Listening to messages in the channel
 for item in client_channel.listen():    
@@ -21,12 +26,16 @@ for item in client_channel.listen():
 
     # For every message received in the channel, converting the bytes to a 2D array.
     frame = np.frombuffer(item["data"], dtype="uint8").reshape(360, 640, 3)
-
+    
+    #writing 
+    out.write(frame)
 
     # Displaying this frame back to the User client.
-    cv2.imshow("Frame", frame)
+    cv2.imshow("VideoFrame", frame)
+    
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
- 
+
 client_channel.unsubscribe()
+
 cv2.destroyAllWindows()
